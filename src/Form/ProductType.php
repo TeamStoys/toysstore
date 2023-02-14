71a45d9fe1b288c674e27cd20af5e59ef3f78397
Ldtkhang <tkth0306@gmail.com>
2023-02-14 18:29:18 +0700

    123

---
+++ src/Form/ProductType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Car;
+use App\Entity\Supplier;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class  ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('name')
+        ->add('price')
+        ->add('created',DateType::class,[
+            'widget' =>'single_text','required'=> false
+        ])
+        ->add('quantity')
+        ->add('file', FileType::class, [
+            'label' => 'Product Image', 'required' => false, 'mapped'=>false]
+        )
+        ->add('image',HiddenType::class,[
+            'required'=> false
+        ])
+        ->add('save',SubmitType::class,[
+            'label' => "Cofirm"
+        ]);
+    }
+}
+?>
